43974c9ea32c5476695f25d2bf15bcd939be941c
Slava206 <slava.tepaev@yandex.ru>
2026-06-30 22:38:26 +0300

    эксперименты

--- train_yolo.py
+++ train_yolo.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from ultralytics import YOLO
 
 # Добавляем путь к src
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
@@ -8,45 +9,36 @@ from src.models.yolo import YOLOv8Model
 from src.utils.utils import get_device, set_seed
 
 def main():
-    # Настройки
     DATA_YAML = 'data/data.yaml'
-    EPOCHS = 10
+    EPOCHS = 25
     BATCH_SIZE = 8
     IMG_SIZE = 640
-    
-    # Фиксируем seed для воспроизводимости
-    set_seed(42)
+
     device = get_device()
-    
-    print(f"Устройство: {device}")
-    
-    # Создаем модель
-    model = YOLOv8Model(
-        model_name='yolov8n.pt',
-        num_classes=7,
-        device=device
-    )
-    
-    # Обучение
-    model.train(
-        data_yaml=DATA_YAML,
-        epochs=EPOCHS,
-        batch_size=BATCH_SIZE,
-        imgsz=IMG_SIZE
-    )
-    
-    # Оценка
-    metrics = model.evaluate(
-        data_yaml=DATA_YAML,
-        batch_size=BATCH_SIZE,
-        imgsz=IMG_SIZE
-    )
-    
-    # Сохранение
-    model.save('results/models/yolov8_best.pt')
-    
-    print("Модель: results/models/yolov8_best.pt")
-    print("Графики: results/logs/yolo/exp/")
+    set_seed(42)
+
+    # Путь к файлу с последней контрольной точкой
+    last_weights_path = 'results/logs/yolo/exp/weights/last.pt'
+
+    # Проверяем есть ли уже сохраненная модель
+    if os.path.exists(last_weights_path):
+        print(f"Найдена контрольная точка: {last_weights_path}")
+        print("Возобновляем обучение...")
+        model = YOLO(last_weights_path)
+        model.train(resume=True)
+    else:
+        print("Запуск обучение с нуля...")
+        model = YOLOv8Model(
+            model_name='yolov8n.pt',
+            num_classes=7,
+            device=device
+        )
+        model.train(
+            data_yaml=DATA_YAML,
+            epochs=EPOCHS,
+            batch_size=BATCH_SIZE,
+            imgsz=IMG_SIZE
+        )
 
 if __name__ == "__main__":
     main()
